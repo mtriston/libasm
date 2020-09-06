751df69c3826212223d3f7f275ebb465611ed9b0
Alexander Spivak <po4ta.polevaya@gmail.com>
2020-09-06 11:59:34 +0300

    refactor function

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -3,34 +3,27 @@ global _ft_strcmp
 section .text
 
 _ft_strcmp:
-			xor		rcx, rcx				; rcx = 0; (counter)
-			cmp		rdi, rsi				; if s1 == s2	
-			jz		equal					;	return (0)
-while:
-			cmp		byte [rsi + rcx], 0		; if (s1[i] == 0)
-			jz		compare					; go to 'compare' label
-			cmp		byte [rdi + rcx], 0		; if (s2[i] == 0)
-			jz		compare					; go to 'compare' label
-			mov		dl, byte [rdi + rcx]	; temp = s1[i]
-			cmp		dl, byte [rsi + rcx]	; if (temp != s2[i])
-			jg		bigger					;	if temp > s[2], go to 'bigger' label
-			jl		lower					;	if temp < s[2], go to 'lower' label
-			inc		rcx						; rcx++ (counter++)
-			jmp		while					; go to 'while' label (cycle)
-compare:
-			mov		dl, byte [rdi + rcx]	; temp = s1[i]
-			cmp		dl, byte [rsi + rcx]	; compare s1[i] and s2[i]
-			jz		equal					; if s1[i] == s2[i], go to 'equal'
-			jg		bigger					; if s1[i] > s2[i], go to 'bigger'
-			jl		lower					; if s1[i] < s2[i], go to 'lower'
+		xor rax, rax			; rax = 0
+		xor rdx, rdx			; rdx = 0
 
-bigger:
-			mov		rax, 1					; return (1);
-			ret
+loop:	mov al, byte [rdi]		; al = dest[i]
+		mov	dl, byte [rsi]		; dl = src[i]
+		sub	rax, rdx			; al - dl
+		jnz	endloop				; if non zero go to endloop
+		test dl, dl				; if src[i] == 0
+		jz endloop				; 	go to endloop
+		inc rdi					; increment dest
+		inc rsi					; increment src
+		jmp loop
+endloop:
+		cmp rax, 0
+		jl lower
+		jg bigger
+		ret
 lower:
-			mov		rax, -1					; return (-1);
-			ret
-equal:
-			mov		rax, 0					; return (0);
-			ret
-
+		mov	rax , -1
+		ret
+bigger:
+		mov rax, 1
+		ret
+		
